ada7ccdbe78dff6e3d8464b7db8357ec6e805049
Aki Mimoto <aki@bako.ca>
2010-01-03 00:16:57 -0800

    Lots of small changes so that we can view this app on a server. not done yet...

--- www/js/main.js
+++ www/js/main.js
@@ -317,7 +317,7 @@ function dialog_load ( title, page ) {
 // Load the appropriate dialog into the space
 //
   $('#dialog').load(
-                    page,
+                    "dialog/"+page,
                     null,
                     function () {
                       $('#dialog').dialog('option','title',title);
